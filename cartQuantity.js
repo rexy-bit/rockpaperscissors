57791fbd54b61e87d2c836a6877d44487a10d6d4
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-02-11 23:11:31 +0100

    Add files via upload

--- cartQuantity.js
+++ cartQuantity.js
@@ -1,31 +1,31 @@
-let cartQuantity = JSON.parse(localStorage.getItem('cartQuantity'));
+let cartQuantity = 0;
 
 function addToCart(value){
-      
-    if(cartQuantity + value <= 10){
+
+    
+
+    if(cartQuantity + value  <= 10){
         cartQuantity += value;
-
     }else if(cartQuantity + value > 10){
-        document.querySelector('.display-error-message').innerHTML = `Your cart is full`;
-
-        
+        document.querySelector('.display-error-message').innerHTML = `Sorry your cart is full`;
     }
 
-    document.querySelector('.display-cart-quantity').innerHTML = `Cart quantity is : ${cartQuantity}`;
-
-    localStorage.setItem('cartQuantity', JSON.stringify(cartQuantity));
+    document.querySelector('.display-cart').innerHTML = `Cart quantity is : ${cartQuantity}`;
 
 }
 
-function removeFromCart(value) {
-    if(cartQuantity + value  >= 0){
+function removeFromCart(value){
+
+     
+    if(cartQuantity + value >= 0 && cartQuantity <= 10){
         cartQuantity += value;
-    }else if(cartQuantity + value <= 0){
-        document.querySelector('.display-error-message').innerHTML = `Your cart is empty, add product !`;
+        
+    }else if(cartQuantity + value < 0){
+        document.querySelector('.display-error-message').innerHTML = `Sorry your cart is empty update your items`;
     }
+    
 
-    document.querySelector('.display-cart-quantity').innerHTML = `Cart quantity is : ${cartQuantity}`;
-
-    localStorage.setItem('cartQuantity', JSON.stringify(cartQuantity));
+    document.querySelector('.display-cart').innerHTML = `Cart quantity is : ${cartQuantity}`;
+    
 
 }
