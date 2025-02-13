0d0535647533a71c439d7547069193c4e029cda6
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-02-13 23:22:45 +0100

    Add files via upload

--- todoList.js
+++ todoList.js
@@ -1,67 +1,70 @@
-let todoList = JSON.parse(localStorage.getItem("todoList")) ||
- [{
-    name : "make dinner",
-    date : "09-02-2025"
-},{
-    name : "Watch youtube",
-    date : "09-02-2025"
-}
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [
+    {
+        name : "Go to the grocery store",
+        date : "2025-02-14"
+    },
+    {
+        name : "Cleane the house",
+        date : "2025-02-14"
+    }
 ];
 
+const addButton = document.querySelector(".add");
 
-
-let addButton = document.querySelector(".add-todo");
 
 addButton.addEventListener("click", ()=>{
     addTodo();
 });
 
-renderTodoList();
-
-
-
 function renderTodoList(){
+     
     let todoListHTML = "";
 
     todoList.forEach((todoObject, i)=>{
+
         let name = todoObject.name;
         let date = todoObject.date;
 
         const html = `
-           <div class="displays2">
-                <div class="name">
-                   ${name}
-                </div>
-                <div class="date">
-                   ${date}
-                </div>
-
-                 <button class="delete-button"
-                   onclick="
-                     todoList.splice(${i}, 1);
-                     renderTodoList();
-                   "
-                 >
-                    Delete
-                 </button>
-            </div>
-        `;
-        todoListHTML += html;
-
         
+           <div class="displays">
+            <div class="name">${name}</div>
+            <div class="date">${date}</div>
+            <button class="delete" data-index = "${i}">Delete</button>
+         </div>     
 
+        `;
+
+        todoListHTML += html;
     });
 
-    document.querySelector(".displays").innerHTML = todoListHTML;
-        
-        localStorage.setItem("todoList", JSON.stringify(todoList));
+    
+    document.querySelector(".display").innerHTML = todoListHTML;
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+
+
+
+
+    document.querySelectorAll(".delete").forEach((button)=>{
+        button.addEventListener("click", (event)=>{
+            let index = event.target.dataset.index;
+
+            todoList.splice(index, 1);
+            renderTodoList();
+        });
+    });
+
+    
+
+    
 }
+
 function addTodo(){
 
     let nameInput = document.querySelector(".input-bar");
-    let name = nameInput.value;
-
     let dateInput = document.querySelector(".input-date");
+
+    let name = nameInput.value;
     let date = dateInput.value;
 
     todoList.push({
@@ -69,10 +72,15 @@ function addTodo(){
         date
     });
 
-      renderTodoList();
-     
+    renderTodoList();
 
     nameInput.value = "";
 
 
+    
 }
+
+
+
+
+
